0ff29b079fa2a7c50f08cfbe4389b5294ba5204d
Max Eriksson <teratoscincus@hotmail.com>
2025-09-04 17:42:50 +0200

    feat(routing): Route to `CocktailCookbookIndex`

--- src/CocktailCookbook.jsx
+++ src/CocktailCookbook.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import CocktailCookbookIndex from "./cocktails/components/CocktailCookbookIndex";
+import { createAlphabetList } from "./cocktails/helpers";
 
 function CocktailCookbook() {
+  const alphabet = createAlphabetList();
+
   return (
-    <section>
-      <h1>Cocktail Cookbook</h1>
-    </section>
+    <Router>
+      <section>
+        <h1>Cocktail Cookbook</h1>
+        <nav>
+          {alphabet.map((letter) => (
+            <Link key={letter} to={`/cocktails/${letter.toLowerCase()}`}>
+              {letter}
+            </Link>
+          ))}
+        </nav>
+        <Routes>
+          <Route path="/" element={<CocktailCookbookIndex />}></Route>
+          <Route
+            path="/cocktails/:indexLetter"
+            element={<CocktailCookbookIndex />}
+          />
+        </Routes>
+      </section>
+    </Router>
   );
 }
 
