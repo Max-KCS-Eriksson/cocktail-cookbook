231f27ec7e0026a9fe32cdc398611822e0108b96
Max Eriksson <teratoscincus@hotmail.com>
2025-09-08 15:32:02 +0200

    feat(favorites): load locally stored `"favorites`"

--- src/CocktailCookbook.jsx
+++ src/CocktailCookbook.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 import CocktailCookbookIndex from "./cocktails/components/CocktailCookbookIndex";
@@ -6,6 +6,26 @@ import { createAlphabetList } from "./cocktails/helpers";
 import CocktailDetails from "./cocktails/components/CocktailDetails";
 
 function CocktailCookbook() {
+  const [favorites, setFavorites] = useState(() => {
+    const saved = localStorage.getItem("favorites");
+    return saved ? JSON.parse(saved) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+  const toggleFavorite = (cocktail) => {
+    setFavorites((currentFavorites) => {
+      const isFavorite = currentFavorites.find(
+        (favorite) => favorite.idDrink === cocktail.idDrink,
+      );
+      if (isFavorite)
+        return currentFavorites.filter(
+          (favorite) => favorite.idDrink !== cocktail.idDrink,
+        );
+      else return [...currentFavorites, cocktail];
+    });
+  };
+
   const alphabet = createAlphabetList();
 
   return (
