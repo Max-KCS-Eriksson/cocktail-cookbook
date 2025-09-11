c0d7f25c702e24f25411799736dd6af77e7a3552
Max Eriksson <teratoscincus@hotmail.com>
2025-09-11 23:39:52 +0200

    feat(ui): Style `FavoriteButton` dynamically

--- src/cocktails/components/FavoriteButton.jsx
+++ src/cocktails/components/FavoriteButton.jsx
@@ -3,13 +3,19 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar as solidStar } from "@fortawesome/free-solid-svg-icons";
 import { faStar as regularStar } from "@fortawesome/free-regular-svg-icons";
 
+import "./FavoriteButton.css";
+
 function FavoriteButton({ cocktail, favorites, toggleFavorite }) {
   const isFavorite = favorites.some(
     (favorite) => favorite.idDrink === cocktail.idDrink,
   );
 
   return (
-    <button onClick={() => toggleFavorite(cocktail)}>
+    <button
+      id="favorite-button"
+      className={`${isFavorite ? "is-favorite" : ""}`}
+      onClick={() => toggleFavorite(cocktail)}
+    >
       <FontAwesomeIcon icon={isFavorite ? solidStar : regularStar} />
     </button>
   );
