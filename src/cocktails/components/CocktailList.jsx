b4b513831b634967c2af8a1fb9cc1d3801367ff0
Max Eriksson <teratoscincus@hotmail.com>
2025-09-12 17:30:59 +0200

    refactor(ui): Cocktail list styling

--- src/cocktails/components/CocktailList.jsx
+++ src/cocktails/components/CocktailList.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 
 import FavoriteButton from "./FavoriteButton";
 
+import "./CocktailList.css";
+
 function CocktailList({ list, favorites, toggleFavorite }) {
   return (
     <ul className="cocktail-list ">
