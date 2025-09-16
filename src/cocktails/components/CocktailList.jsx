7c3d3099acf8405b09ead6b91f7ff4fd19241767
Max Eriksson <teratoscincus@hotmail.com>
2025-09-16 22:35:02 +0200

    feat(ui-thumbnails): Thumbnail in list view

--- src/cocktails/components/CocktailList.jsx
+++ src/cocktails/components/CocktailList.jsx
@@ -4,19 +4,27 @@ import { Link } from "react-router-dom";
 import FavoriteButton from "./FavoriteButton";
 
 import "./CocktailList.css";
+import CocktailThumbnail from "./CocktailThumbnail";
 
 function CocktailList({ list, favorites, toggleFavorite }) {
   return (
     <ul className="cocktail-list ">
       {list.map((cocktail) => (
-        <li key={cocktail.idDrink}>
-          <FavoriteButton
-            cocktail={cocktail}
-            favorites={favorites}
-            toggleFavorite={toggleFavorite}
-          />
-          <Link to={`/cocktail/${cocktail.idDrink}`}>{cocktail.strDrink}</Link>
-        </li>
+        <>
+          <li key={cocktail.idDrink}>
+            <FavoriteButton
+              cocktail={cocktail}
+              favorites={favorites}
+              toggleFavorite={toggleFavorite}
+            />
+            <Link to={`/cocktail/${cocktail.idDrink}`}>
+              {cocktail.strDrink}
+            </Link>
+          </li>
+          <Link to={`/cocktail/${cocktail.idDrink}`}>
+            <CocktailThumbnail cocktail={cocktail} small={true} />
+          </Link>
+        </>
       ))}
     </ul>
   );
